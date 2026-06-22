df4e980d7b19e52e0b0a254c1aaabc7c7ad209c3
Musa-Abdusamed <198797287+Musa-Abdusamed@users.noreply.github.com>
2026-06-22 12:57:28 +0300

    Add sortColors method to sort colors array
    Implement sortColors method to sort an array of colors in-place.

---
+++ Leetcode/Array/Array/75.sort colors.py
@@ -0,0 +1,10 @@
+#Solved by ma
+
+
+class Solution: 
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        l1=nums.sort()
+        return l1
